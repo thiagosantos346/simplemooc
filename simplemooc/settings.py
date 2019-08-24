ee5c797488fbd2876b86b6031544c16dd32ed220
thiagosantos346 <thiagosantos346@gmail.com>
2019-08-23 22:30:25 -0300

    d

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
+MEDIA_URL = '/media/'
 
 #E-MAILS
 
