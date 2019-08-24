39743884e985dafed7b4bdaf4030a5b69830ea16
thiagosantos346 <thiagosantos346@gmail.com>
2019-08-23 22:23:18 -0300

    KEY CONFIGURED

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hp_f&(vkhb9!%syknk02me%1@e#@7-^!t&+u6pq5dpkqwqs#-#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
